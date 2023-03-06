74abef237421745f3e49ab397456fa92f73d612a
Rifqi Naufal <rifqi.nufaltohari@gojek.com>
2023-03-06 13:21:13 +0700

    implement Command Line Runner to print room list into log

---
+++ clr-application/src/test/java/com/toharifqi/clr/clrapplication/ClrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toharifqi.clr.clrapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
